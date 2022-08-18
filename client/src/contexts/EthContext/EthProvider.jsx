476398143c758b95534c77e6bb224893f7e517e1
Kapil-Mulay-1421 <krmulay@gmail.com>
2022-08-18 14:29:40 +0530

    network check

--- client/src/contexts/EthContext/EthProvider.jsx
+++ client/src/contexts/EthContext/EthProvider.jsx
@@ -12,6 +12,9 @@ function EthProvider({ children }) {
         const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
         const accounts = await web3.eth.requestAccounts();
         const networkID = await web3.eth.net.getId();
+        if (networkID != 80001) {
+          alert("You are not connected to the Mumbai Test Network.")
+        }
         const { abi } = artifact;
         let address, contract;
         try {
